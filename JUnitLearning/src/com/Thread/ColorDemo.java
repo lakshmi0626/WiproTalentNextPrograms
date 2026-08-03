a2439d6e241a9d503c1d6f83a021267228b45fc4
lakshmi0626 <lakshmithirupathamma169@gmail.com>
2026-08-03 19:29:10 +0530

    Add files via upload

---
+++ JUnitLearning/src/com/Thread/ColorDemo.java
@@ -0,0 +1,37 @@
+package com.Thread;
+
+import java.util.Random;
+
+class ColourThread implements Runnable {
+
+    public void run() {
+
+        String colours[] = {"White", "Blue", "Black", "Green", "Red", "Yellow"};
+
+        Random random = new Random();
+
+        while (true) {
+
+            int index = random.nextInt(colours.length);
+
+            System.out.println(colours[index]);
+
+            if (colours[index].equals("Red")) {
+                System.out.println("Red colour found. Stopping...");
+                break;
+            }
+        }
+    }
+}
+
+public class ColorDemo {
+
+    public static void main(String[] args) {
+
+        ColourThread obj = new ColourThread();
+
+        Thread t = new Thread(obj);
+
+        t.start();
+    }
+}
